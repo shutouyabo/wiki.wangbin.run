df6feba165c9d6f24ab482799e8688752f3aebdf
彬少 <dz303241@gmail.com>
2026-02-04 14:36:38 +0800

    更新

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'junjieweb', // Usually your GitHub org/user name.
-    projectName: 'junjieweb.github.io', // Usually your repo name.
+    organizationName: 'wiki', // Usually your GitHub org/user name.
+    projectName: 'wiki.wangbin.run', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
